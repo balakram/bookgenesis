fdee13fa3a0dab83d2655711ff18269cc3de64f8
BALAKRAM TUDU <48530348+balakram@users.noreply.github.com>
2024-07-04 22:05:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var modal = document.getElementById("myModal");
+
+function showModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+function verifyCode() {
+  var code = document.getElementById('code').value;
+
+  if (code === '01012001') { // Change this to your desired verification code
+    window.location.href = 'main-page.html'; // Change 'main-page.html' to the URL of your main page
+  } else {
+    var errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = 'Incorrect verification code. Please try again.';
+  }
+}
+function getCode() {
+  var code = document.getElementById('code').value;
+
+  if (code === '1111') { // Change this to your desired verification code
+    window.location.href = 'main-page.html'; // Change 'main-page.html' to the URL of your main page
+  } else {
+    var errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = 'Contact to BALAKRAM TUDU.';
+  }
+}
