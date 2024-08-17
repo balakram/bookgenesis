ed9b481378e6068b4b03f583dc812fffe33b916c
BALAKRAM TUDU <48530348+balakram@users.noreply.github.com>
2024-08-17 20:55:28 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,20 @@ window.addEventListener("scroll", function () {
 });
 
 
+/**
+ * Cursor
+ */
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function (event) {
+  // event.pageX --> across
+  // event.pageY --> up and down
+  // console.log(event);
+  moveCursor(event.pageX, event.pageY);
+});
+
+const moveCursor = function (pageX, pageY) {
+  cursor.style.left = pageX + "px";
+  cursor.style.top = pageY + "px";
+};
+
